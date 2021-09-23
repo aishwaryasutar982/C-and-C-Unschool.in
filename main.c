9c3863a7e3820803ba5ced164de2b52fd7c17db4
aishwaryasutar982 <91255608+aishwaryasutar982@users.noreply.github.com>
2021-09-23 15:31:27 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,sum;
+    printf("Enter a Three no.:");
+    scanf("%d %d %d",&a,&b,&c);
+
+    sum=a+b+c;
+    printf("Sum = %d",sum);
+    return 0;
+}
